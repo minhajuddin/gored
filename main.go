31f00ae3c35b318b3f7c59533805001621279e33
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2012-11-28 23:33:18 +0530

    Added ability to make a GET in an ugly way

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ type Redis struct {
 }
 
 func (self *Redis) run(args ...string) {
+	//TODO: Move this into a command builder
 	//build command
 	cmd := "*" + strconv.Itoa(len(args)) + "\r\n"
 	for _, t := range args {
@@ -40,6 +41,18 @@ func (self *Redis) Set(key string, value string) error {
 	}
 	log.Println(line)
 	return nil
+}
+
+//TODO: Create a result parser
+func (self *Redis) Get(key string) (string, error) {
+	self.run("GET", key)
+	self.Reader.ReadString('\n')
+	//status, err := self.Reader.ReadString('\n')
+	op, err := self.Reader.ReadString('\n')
+	if err != nil {
+		return "", err
+	}
+	return strings.Trim(op, "+\r\n"), nil
 }
 
 func (self *Redis) Close() {
